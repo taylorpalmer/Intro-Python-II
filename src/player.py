e88291755dfb871b12f2abac38966d428f915e56
Taylor Palmer <taylorjadepalmer@gmail.com>
2020-08-13 16:57:39 -0700

    refactored to reflect help given during lecture

--- src/player.py
+++ src/player.py
@@ -1,14 +1,9 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+from room import Room
+
+
 class Player:
-    def __init__(self, room='outside', ???):
-        self.room = room
-
-    def __str__(self):
-        return f"{self.room}"
-
-    def move(d):
-        if
-
-    d = input("Choose a direction! (n, s, e, w)")
-    d = str(d)
+    def __init__(self, current_room):
+        self.current_room: Room = current_room
--- src/room.py
+++ src/room.py
@@ -4,6 +4,12 @@ class Room:
     def __init__(self, name, description):
         self.name = name
         self.description = description
+        self.n_to: Room = None
+        self.s_to: Room = None
+        self.e_to: Room = None
+        self.w_to: Room = None
 
-    def __str__(self):
-        return f"{self.name}, {self.description}"
+# 1) Adding these as attributes in the constructor is optional;
+# in python you can arbitrarily set attributes on instances
+# 2) the `: Room` syntax is a typehint. It just serves as a reminder
+# for other developers that self.n_to is storing a Room object (as opposed to a str)
